ce047fd8e8c0dec5c509627837dd0cd6d8db1722
Jason May <jason.a.may@gmail.com>
2011-06-12 21:27:23 -0400

    docs

--- lib/App/Termcast/Connector.pm
+++ lib/App/Termcast/Connector.pm
@@ -6,10 +6,25 @@ use IO::Socket::UNIX;
 use JSON ();
 use Try::Tiny;
 
+=attr manager_socket_path
+
+The path to the listening manager UNIX socket that L<App::Termcast::Server>
+creates.
+
+=cut
+
 has manager_socket_path => (
     is       => 'ro',
     isa      => 'Str',
 );
+
+=attr manager_socket
+
+UNIX socket that connects to L<App::Termcast::Server>'s manager socekt. By
+default this is automatically built using the C<manager_socket_path> attribute
+for the path.
+
+=cut
 
 has manager_socket => (
     is      => 'ro',
@@ -28,13 +43,18 @@ sub _build_manager_socket {
     return $socket;
 }
 
-sub request_sessions {
-    my $self = shift;
-    my $socket = shift || $self->manager_socket;
+=attr sessions_cb
 
-    my $req_string = JSON::encode_json({request => 'sessions'});
-    $socket->syswrite($req_string);
-}
+When C<< <$connector->request_sessions> >> is called`, the server will respond
+with the sessions available. This callback will be invoked when a response
+comes back. The C<App::Termcast::Connector> object, followed by the list
+of sessions will be passed into the callback.
+
+=method register_sessions_callback
+
+Assigns a coderef to the C<sessions_cb> attribute.
+
+=cut
 
 has sessions_cb => (
     is     => 'rw',
@@ -42,17 +62,54 @@ has sessions_cb => (
     writer => 'register_sessions_callback',
 );
 
+=attr connect_cb
+
+When a new broadcaster connects to the server, it lets all app connections know.
+In this case, the C<connect_cb> callback is called. The connector object
+followed by a hashref of the connection data is passed into this callback.
+
+=method register_connect_callback
+
+Assigns a coderef to the C<connect_cb> attribute.
+
+=cut
+
 has connect_cb => (
     is     => 'ro',
     isa    => 'CodeRef',
     writer => 'register_connect_callback',
 );
 
+=attr disconnect_cb
+
+When a broadcaster disconnects to the server, it lets all app connections know.
+In this case, the C<disconnect_cb> callback is called. The connector object
+followed by session ID (Str) is passed into this callback.
+
+=method register_disconnect_callback
+
+Assigns a coderef to the C<disconnect_cb> attribute.
+
+=cut
+
 has disconnect_cb => (
     is     => 'ro',
     isa    => 'CodeRef',
     writer => 'register_disconnect_callback',
 );
+
+=attr metadata_cb
+
+When a broadcaster makes certain changes to its terminal, the server lets all
+app connections know.  In this case, the C<metadata_cb> callback is called.
+The connector object, session ID (Str), and the data associated with the
+(HashRef) are passed into this callback.
+
+=method register_metadata_callback
+
+Assigns a coderef to the C<metadata_cb> attribute.
+
+=cut
 
 has metadata_cb => (
     is     => 'ro',
@@ -65,6 +122,47 @@ has json => (
     isa => 'JSON',
     default => sub { JSON->new },
 );
+
+=meth request_sessions
+
+  Usage: $connector->request_sessions
+
+or:
+  
+  Usage: $connector->request_sessions($socket);
+
+Sends a request for a list of the current Termcast sessions. When a response
+coems back, the C<sessions_cb> callback will be triggered.
+
+If you chose not to pass in a C<manager_socket_path> param, you can request
+termcast sessions by passing in your own socket.
+
+=cut
+
+sub request_sessions {
+    my $self = shift;
+    my $socket = shift || $self->manager_socket;
+
+    my $req_string = JSON::encode_json({request => 'sessions'});
+    $socket->syswrite($req_string);
+}
+
+=meth dispatch
+
+  Usage: $connector->dispatch($input);
+
+or:
+
+  Usage: $connector->dispatch($ref, decoded => 1);
+
+When you get any input from the Termcast server's manager socket, you pass it
+in to this method.
+
+You can pass C<< <decoded => 1> >> if you are using something that can already
+decodes JSON for you, such as L<AnyEvent::Handle>'s C<push_write>. Otherwise
+C<dispatch> will do the decoding for you.
+
+=cut
 
 sub dispatch {
     my $self   = shift;
@@ -130,6 +228,23 @@ sub handle_response {
         }
     }
 }
+
+=meth make_socket
+
+  Usage: $connector->dispatch($input);
+
+or:
+
+  Usage: $connector->dispatch($ref, decoded => 1);
+
+When you get any input from the Termcast server's manager socket, you pass it
+in to this method.
+
+You can pass C<< <decoded => 1> >> if you are using something that can already
+decodes JSON for you, such as L<AnyEvent::Handle>'s C<push_write>. Otherwise
+C<dispatch> will do the decoding for you.
+
+=cut
 
 sub make_socket {
     my $self = shift;
